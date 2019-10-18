ccd5205f41415fee83de44db35784a7cdbb3e58b
ABoK4Do <alesha148@gmail.com>
2019-10-18 23:25:41 +0300

    LockCounter

--- src/main/java/counter/LockCounter.java
+++ src/main/java/counter/LockCounter.java
@@ -1,13 +1,24 @@
 package counter;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class LockCounter implements Counter {
+    private Lock lock = new ReentrantLock();
+    private volatile long value = 0;
+
     @Override
     public void increment() {
-
+        lock.lock();
+        value++;
+        lock.unlock();
     }
 
     @Override
     public long getValue() {
-        return 0;
+        lock.lock();
+        long result = value;
+        lock.unlock();
+        return result;
     }
 }
